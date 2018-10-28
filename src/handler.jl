41408a4378fe993eb464c36f6aaea8a656d8409f
Emily A Grasmeder <emma@gras.org>
2018-10-28 23:28:06 +0100

    Display font correctly

---
+++ src/handler.jl
@@ -0,0 +1,32 @@
+using Mux
+using Logging
+using Dates
+
+include("functions.jl")
+include("cards.jl")
+io = open("log.txt", "w+")
+
+global_logger(SimpleLogger(io))
+
+default = "<h1>Hello World</h1>"
+
+@app test = (
+  Mux.defaults,
+  page("/", respond(default)),
+  page("/cards", respond(deal())),
+  page("/about", respond("This is just Another Flashcard App")),
+  Mux.notfound())
+
+serve(test)
+Base.JLOptions().isinteractive == 0 && wait()
+
+
+
+
+
+# initialize()
+
+#@info(now(), status="incorrect", language_from="english", language_to="hebrew", entry=card)
+
+
+card = randrecord()
