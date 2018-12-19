aabd2fc50913a25d5c289af71d8459035c87d3fa
Emily A Grasmeder <emma@gras.org>
2018-12-19 23:52:47 +0100

    remove reagent frontend

--- reagent-frontend/env/prod/clj/reagent_frontend/middleware.clj
+++
@@ -1,5 +0,0 @@
-(ns reagent-frontend.middleware
-  (:require [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
-
-(def middleware
-  [#(wrap-defaults % site-defaults)])
--- reagent-frontend/env/prod/cljs/reagent_frontend/prod.cljs
+++
@@ -1,7 +0,0 @@
-(ns reagent-frontend.prod
-  (:require [reagent-frontend.core :as core]))
-
-;;ignore println statements in prod
-(set! *print-fn* (fn [& _]))
-
-(core/init!)
