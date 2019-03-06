5dcaa7ab7c4aa88ca6a696c30d0a341cf0c67d99
Emily A Grasmeder <emma@gras.org>
2019-03-06 19:14:05 +0100

    lint and use prop types because the linter yelled at me

--- frontend/src/Button.js
+++ frontend/src/Button.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Button.css';
 
 class Button extends Component {
   render() {
     return (
-      <button
-        className={"Button"}
-        onClick={this.props.onClick}
-      >
+      <button className={'Button'} onClick={this.props.onClick}>
         {this.props.text}
-      </button >
+      </button>
     );
   }
 }
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+Button.defaultProps = {
+  text: '',
+  onClick: e => e
+};
 
 export default Button;
